cbd016df38fc88b1897dddaa3547ed19b043317d
Lucas Tiago <eulukasthyago@outlook.com>
2023-09-02 09:45:50 -0300

    Ajustes finais do mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { AlertArea, Card, CardSection } from "./components";
 import toast, { Toaster } from "react-hot-toast";
 
@@ -17,12 +17,9 @@ export default function Home() {
   const resumeRef = useRef<HTMLTextAreaElement>(null);
   const numberSectionRef = useRef<HTMLSelectElement>(null);
   const numberParagraphRef = useRef<HTMLSelectElement>(null);
+  const stepBarRef = useRef<HTMLDivElement>(null);
+  const initialApp = useRef(true);
 
-  const alertTitle = useRef("Copiado com sucesso.");
-  const alertMessage = useRef("O conteúdo selecionado, foi copiado com sucesso.");
-  const alertType = useRef("success");
-
-  const [alertShow, setAlertShow] = useState(false);
   const [step, setStep] = useState(1);
 
   const handleAutoSelect = (e: React.FormEvent<HTMLTextAreaElement>) => {
@@ -31,11 +28,20 @@ export default function Home() {
       e.currentTarget.select();
       e.currentTarget.setSelectionRange(0, 99999);
       navigator.clipboard.writeText(e.currentTarget.value);
-      handleShowArlet("success", "Copiado com sucesso.", "O conteúdo selecionado, foi copiado com sucesso.");
+      handleShowArlet("success", "O conteúdo selecionado, foi copiado com sucesso.");
     }
   };
 
-  const handleShowArlet = (type: string, title: string, message: string) => {
+  const handleScroolTo = () => {
+    const activeStep: HTMLDivElement | undefined | null = stepBarRef.current?.querySelector("[data-active='active']");
+    if (stepBarRef.current && activeStep) {
+      stepBarRef.current.scroll({ left: activeStep.offsetLeft - 20, behavior: "smooth" });
+      window.scroll({ top: stepBarRef.current.offsetTop - 80, behavior: "smooth" });
+    }
+    console.log(activeStep);
+  };
+
+  const handleShowArlet = (type: string, message: string) => {
     switch (type) {
       case "success":
         toast.success(message);
@@ -54,10 +60,6 @@ export default function Home() {
     // alertType.current = type;
     // setAlertShow(true);
     // setTimeout(handleCloseAlert, 3500);
-  };
-
-  const handleCloseAlert = (): void => {
-    setAlertShow(false);
   };
 
   const handleGeneratePrompt = () => {
@@ -79,24 +81,34 @@ export default function Home() {
         case 2:
           if (topcRef.current?.value !== "")
             titlePromptRef.current.value = `Write a title for an article about "${topcRef.current.value}" in ${languageRef.current.value}. Style: ${writingStyleRef.current.value}. Tone: ${writingToneRef.current.value}. Must be between 40 and 60 characters.`;
-          else handleShowArlet("error", "Campo não preenchido.", "Por favor, preencha o campo de Tópico.");
+          else handleShowArlet("error", "Por favor, preencha o campo de Tópico.");
           break;
         case 3:
           if (topcRef.current?.value !== "" && titleRef.current?.value !== "")
             sectionPromptRef.current.value = `Write ${numberSectionRef.current.value} consecutive headings for an article about "${titleRef.current.value}", in ${languageRef.current.value}. Style: ${writingStyleRef.current.value}. Tone: ${writingToneRef.current.value}. Each heading is between 40 and 60 characters. Use Markdown for the headings (## ).`;
-          else handleShowArlet("error", "Campo não preenchido.", "Por favor, preencha o campo de Título do artigo.");
+          else handleShowArlet("error", "Por favor, preencha o campo de Título do artigo.");
           break;
         case 4:
           if (topcRef.current?.value !== "" && titleRef.current?.value !== "" && sectionRef.current?.value !== "") {
             contentRef.current.value = `Write an article about "${titleRef.current.value}" in ${languageRef.current.value}. The article is organized by the following headings:\n\r${sectionRef.current.value}\n\rWrite ${numberParagraphRef.current.value} paragraphs per heading. Use Markdown for formatting. Add an introduction prefixed by "===INTRO: ", and a conclusion prefixed by "===OUTRO: ". Style: ${writingStyleRef.current.value}. Tone: ${writingToneRef.current.value}.`;
             resumeRef.current.value = `Write an excerpt for an article about "${titleRef.current.value}" in ${languageRef.current.value}. Style: ${writingStyleRef.current.value}. Tone: ${writingToneRef.current.value}. Must be between 40 and 60 characters.`;
-          } else handleShowArlet("error", "Campo não preenchido.", "Por favor, preencha o campo de seções do artigo.");
+          } else handleShowArlet("error", "Por favor, preencha o campo de seções do artigo.");
           break;
         case 5:
           break;
       }
     }
   };
+
+  useEffect(() => {
+    if (initialApp.current){
+      initialApp.current = false;
+      return;
+    }
+    if(window.innerWidth <= 768){
+      handleScroolTo();
+    }
+  }, [step]);
 
   const handleNextStep = () => {
     switch (step) {
@@ -105,15 +117,15 @@ export default function Home() {
         break;
       case 2:
         if (topcRef.current?.value !== "") setStep(3);
-        else handleShowArlet("error", "Campo não preenchido.", "Por favor, preencha o campo de Tópico.");
+        else handleShowArlet("error", "Por favor, preencha o campo de Tópico.");
         break;
       case 3:
         if (topcRef.current?.value !== "" && titleRef.current?.value !== "") setStep(4);
-        else handleShowArlet("error", "Campo não preenchido.", "Por favor, preencha o campo de Título do artigo.");
+        else handleShowArlet("error", "Por favor, preencha o campo de Título do artigo.");
         break;
       case 4:
         if (topcRef.current?.value !== "" && titleRef.current?.value !== "" && sectionRef.current?.value !== "") setStep(5);
-        else handleShowArlet("error", "Campo não preenchido.", "Por favor, preencha o campo de seções do artigo.");
+        else handleShowArlet("error", "Por favor, preencha o campo de seções do artigo.");
         break;
       case 5:
         break;
@@ -140,27 +152,20 @@ export default function Home() {
 
   return (
     <>
-      <AlertArea
-        show={alertShow}
-        type={alertType.current}
-        title={alertTitle.current}
-        message={alertMessage.current}
-        closeFunction={handleCloseAlert}
-      />
       <main className="flex overflow-hidden min-h-screen flex-col items-center justify-between p-4 xl:p-24">
-        <div className="z-10 items-center justify-between font-mono text-sm lg:flex w-full max-w-[1300px] mx-auto">
-          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <div className="z-10 items-center justify-between font-mono text-sm md:flex w-full max-w-[1300px] mx-auto">
+          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit md:static md:w-auto  md:rounded-xl md:border md:bg-gray-200 md:p-4 md:dark:bg-zinc-800/30">
             Gerador de prompt para posts
           </p>
-          <div className="fixed bottom-0 left-0 flex h-[80px] w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <div className="flex h-[80px] w-full md:max-w-[300px] pt-20 md:pt-0 justify-center">
             <a
-              className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+              className="pointer-events-none flex place-items-center gap-2 p-8 md:pointer-events-auto md:p-0"
               href="#Lucas."
               target="_blank"
               rel="noopener noreferrer"
               onClick={(e) => {
                 e.preventDefault();
-                handleShowArlet("warning", "Em breve...", `Logo disponibilizarei o link para meu site. \n\r Aguarde...`);
+                handleShowArlet("warning", `Logo disponibilizarei o link para meu site. \n\r Aguarde...`);
               }}
             >
               By <Image src="/lucas_logo_branca.svg" alt="Lucas Logo" className="" width={100} height={24} priority />
@@ -168,19 +173,32 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="pt-20 relative flex place-items-start w-full max-w-[1300px] mx-auto before:absolute before:left-1/2 before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:left-1/3 after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+        <div className="pt-20 relative flex place-items-start w-full max-w-[1300px] mx-auto before:absolute before:left-1/2 before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:left-1/3 after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:md:h-[360px]">
           <div className="z-[1] flex flex-wrap items-start gap-4 w-full">
             <Card className="w-full order-1">
-              <div className="w-full overflow-hidden md:overflow-visible flex items-center gap-[10px] text-lg uppercase whitespace-nowrap text-white/60">
-                <div className={`${step === 1 && "text-[#fe842b] font-bold"}`}>Configurção</div>
+              <div
+                ref={stepBarRef}
+                className="w-full overflow-hidden md:overflow-visible flex items-center gap-[10px] text-lg uppercase whitespace-nowrap text-white/60"
+              >
+                <div data-active={`${step === 1 ? "active" : "disabled"}`} className={`${step === 1 ? "text-[#fe842b] font-bold" : ""}`}>
+                  Configurção
+                </div>
                 <div className="h-[1px] bg-white/20 w-full min-w-[50px] max-w-[300px]"></div>
-                <div className={`${step === 2 && "text-[#fe842b] font-bold"}`}>Titulo</div>
+                <div data-active={`${step === 2 ? "active" : "disabled"}`} className={`${step === 2 ? "text-[#fe842b] font-bold" : ""}`}>
+                  Titulo
+                </div>
                 <div className="h-[1px] bg-white/20 w-full min-w-[50px] max-w-[300px]"></div>
-                <div className={`${step === 3 && "text-[#fe842b] font-bold"}`}>Seções</div>
+                <div data-active={`${step === 3 ? "active" : "disabled"}`} className={`${step === 3 ? "text-[#fe842b] font-bold" : ""}`}>
+                  Seções
+                </div>
                 <div className="h-[1px] bg-white/20 w-full min-w-[50px] max-w-[300px]"></div>
-                <div className={`${step === 4 && "text-[#fe842b] font-bold"}`}>Conteudo</div>
+                <div data-active={`${step === 4 ? "active" : "disabled"}`} className={`${step === 4 ? "text-[#fe842b] font-bold" : ""}`}>
+                  Conteudo
+                </div>
                 <div className="h-[1px] bg-white/20 w-full min-w-[50px] max-w-[300px]"></div>
-                <div className={`${step === 5 && "text-[#fe842b] font-bold"}`}>Resumo</div>
+                <div data-active={`${step === 5 ? "active" : "disabled"}`} className={`${step === 5 ? "text-[#fe842b] font-bold" : ""}`}>
+                  Resumo
+                </div>
               </div>
             </Card>
 
@@ -563,7 +581,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"></div>
+        <div className="mb-32 grid text-center md:max-w-5xl md:w-full md:mb-0 md:grid-cols-4 md:text-left"></div>
       </main>
       <Toaster />
     </>
