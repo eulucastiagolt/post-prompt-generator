8b354174b6f4b5151d0fad30f1ee4342004a32d4
Lucas Tiago <eulukasthyago@outlook.com>
2023-09-02 14:14:06 -0300

    Ajustes de meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,14 +8,14 @@ export const metadata: Metadata = {
   title: "Post Prompt Generator",
   description:
     "Obtenha prompt instantânea para seus posts! O Post Prompt Generator gera prompts para você usar em inteligência artificial e gerar ports que Impulsione seu conteúdo com facilidade!",
+  authors: [{ name: "Lucas Tiago", url: "https://github.com/eulukasthyago" }],
   openGraph: {
     title: "Post Prompt Generator",
     description:
       "Obtenha prompt instantânea para seus posts! O Post Prompt Generator gera prompts para você usar em inteligência artificial e gerar ports que Impulsione seu conteúdo com facilidade!",
     images: ["/thumbnail.png"],
-    authors: "Lucas Tiago",
-    publishedTime: "2023-09-02"
-  }
+    publishedTime: "2023-09-02",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
